8a9cfab3593d3bf3c20e528316551abb3942f6f0
Aaditya Mishra <codewithaadi31@gmail.com>
2023-01-28 14:12:51 +0530

    Database Error Solved.

--- server/database/db.js
+++ server/database/db.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const USERNAME = process.env.DB_USERNAME;
-const PASSWORD = process.env.DB_PASSWORD;
+// const USERNAME = process.env.DB_USERNAME;
+// const PASSWORD = process.env.DB_PASSWORD;
 
 
 export const Connection  = ()=>{
  
 
     mongoose.set('strictQuery', false);
-    const MONGODB_URL = `mongodb+srv://${USERNAME}:${PASSWORD}@mern-todo.jlncukk.mongodb.net/?retryWrites=true&w=majority`
+    // const MONGODB_URL = `mongodb+srv://${USERNAME}:${PASSWORD}@mern-todo.jlncukk.mongodb.net/?retryWrites=true&w=majority`
+
+    const MONGODB_URL = `mongodb+srv://user:user123@mern-todo.jlncukk.mongodb.net/?retryWrites=true&w=majority`
 
     mongoose.connect(MONGODB_URL, {useNewUrlParser : true});
 
